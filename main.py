a80ea074519ec5beb11c2b642a598126e54ec19a
b1leygr <okeferanmi10@gmail.com>
2026-06-27 09:26:20 +0100

    add script for end-to-end fen generation

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+from utils import *
+from pathlib import Path
+from rfdetr import RFDETRMedium
+
 def main():
-    print("Hello from chessify!")
+    project_dir = Path.cwd()
+    print(project_dir)
+    while True:
+        file = input('Choose an image file:') + '.jpg'
+        img = cv2.imread(project_dir / "notebooks" / "game_opera_side" / file)
+        try:
+            img = cv2.imread(project_dir / "notebooks" / "game_opera_side" / file)
+            print(f"{img} found!")
+            break
+        except:
+            print(f"{file} not found. Please choose a different image file.")
+    blur = preprocess(img)
+    canny = autocanny(blur)
+    dilated = dilate (canny)
+    points = find_contour(dilated)
+    corners = find_corners(points)
+    warped, M_inv = warp(img, corners)
+    blur = preprocess(warped)
+    canny = autocanny(blur)
+    closed = close(canny)
+    lines = hough_lines(canny)
+    h, v = sort_lines(lines)
+    intersections = find_intersections(h, v)
+    clustered_points = cluster_intersections(intersections)
+    corners = find_corners(clustered_points)
+    squares = get_squares(corners, M_inv)
+    square_dict = squares_to_dict(squares)
+    model = RFDETRMedium(pretrain_weights=str(project_dir / 'models' / 'rfdetr_medium' / 'checkpoint_best_total.pth'), num_classes=12)
+    predictions = model.predict(img)
+    predict_board(square_dict, predictions)
+    fen = dict_to_fen(square_dict)
+    print(fen)
 
 
 if __name__ == "__main__":
