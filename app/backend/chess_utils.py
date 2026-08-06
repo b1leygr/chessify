49fbb99b987b9d2e44744832f93dbb441effe420
b1leygr <okeferanmi10@gmail.com>
2026-08-06 06:54:59 +0100

    revise fen logic to include alternate permutations

--- app/backend/chess_utils.py
+++ app/backend/chess_utils.py
@@ -1,3 +1,7 @@
+"""
+A module for processing chessboard images and deriving board states.
+"""
+
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
@@ -5,6 +9,7 @@ import scipy.spatial as spatial
 import scipy.cluster as clstr
 from collections import defaultdict
 import operator
+import itertools
 
 def preprocess(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -107,7 +112,7 @@ def get_squares(corners, M_inv):
     # Create meshgrid to get the X,Y coordinates of all intersections
     xv, yv = np.meshgrid(x_points, y_points)
 
-    # Extract and print the 4 corners for each grid square
+    # Derive the four corners of each grid square
     squares = []
     for i in range(rows):
         for j in range(cols):
@@ -123,61 +128,147 @@ def get_squares(corners, M_inv):
     squares = squares.reshape(-1, 4, 2)
     return squares
 
-def squares_to_dict(squares):
-    square_list = []
-    for square in squares:
+def localise_and_extract(img):
+    """Localise a chessboard in an image and extract the grid squares."""
+    blur = preprocess(img)
+    canny = autocanny(blur)
+    dilated = dilate(canny)
+    points = find_contour(dilated)
+    board_corners = find_corners(points)
+    warped, M_inv = warp(img, board_corners)
+    closed = close(autocanny(preprocess(warped)))
+    lines = hough_lines(closed)
+    h, v = sort_lines(lines)
+    intersections = find_intersections(h, v)
+    clustered_points = cluster_intersections(intersections)
+    grid_corners = find_corners(clustered_points)
+    squares = get_squares(grid_corners, M_inv)
+    return squares
+
+def squares_to_dict(squares, perspective='white_left'):
+    """
+    Map chessboard square coordinates to a dictionary with algebraic notation keys.
+    Perspective assumptions:
+    - White is on the left side of the image frame.
+    - Black is on the right side of the image frame.
+    - Top-left: a1 | Top-right: a8 | Bottom-left: h1 | Bottom-right: h8
+    Can be adjusted using the perspective parameter.
+
+    Args:
+        squares (np.ndarray): An array of shape (64, 4, 2) containing image coordinates for 64 squares.
+        perspective (str): The perspective of the board. Options are 'white', 'black', 'white_left', 'white_right'.
+    Returns:
+        A dictionary with keys as square names in algebraic notation (e.g., 'a1', 'h8')
+        and values as dictionaries containing 'predictions' (initialised to None)
+        and 'corners' (square coordinates).
+    """
+    square_dict = {}
+    files = "abcdefgh"
+    ranks = "12345678"
+    if perspective == 'white':
+        # Top-to-bottom: 8 to 1, Left-to-right: a to h
+        square_names = [f'{file}{rank}' for rank in ranks[::-1] for file in files]
+    elif perspective == 'black':
+        # Top-to-bottom: 1 to 8, Left-to-right: h to a
+        square_names = [f'{file}{rank}' for rank in ranks for file in files[::-1]]
+    elif perspective == 'white_left':
+        # Top-to-bottom: a to h, Left-to-right: 1 to 8
+        square_names = [f'{file}{rank}' for file in files for rank in ranks]
+    elif perspective == 'white_right':
+        # Top-to-bottom: h to a, Left-to-right: 8 to 1
+        square_names = [f'{file}{rank}' for file in files[::-1] for rank in ranks[::-1]]
+    for square, square_name in zip(squares, square_names):
         x0, y0 = np.min(square, axis=0)
         x1, y1 = np.max(square, axis=0)
-        square_list.append((float(x0), float(y0), float(x1), float(y1)))
-
-    square_dict = {}
-    cols = "abcdefgh"
-    rows = "12345678"
-    square_names = [f'{col}{row}' for col in cols for row in rows]
-    for square, square_name in zip(square_list, square_names):
-        square_dict[square_name] = {'piece': None, 'corners': square, 'score': None}
+        square_dict[square_name] = {'predictions': [],
+                                    'corners': (float(x0), float(y0), float(x1), float(y1))}
 
     return square_dict
 
-def predict_board(square_dict, predictions):
+def predict_board(square_dict, detections):
+    """
+    Map detections to corresponding squares based on detection and square coordinates.
+
+    Args:
+        square_dict (dict): A dictionary containing square information and predictions.
+        detections (list): A list of detection results.
+    Returns:
+        Updates the square_dict in place with predictions for each square.
+    """
     label_list = ["B", "K", "N", "P", "Q", "R", "b", "k", "n", "p", "q", "r"]
-    for square_name, square_info in square_dict.items():
+    for _, square_info in square_dict.items():
         x0, y0, x1, y1 = square_info['corners']
-        for pred in predictions:
-            xyxy = pred[0]
-            bottom_center = ((xyxy[0] + xyxy[2]) / 2, xyxy[3])
-            label = pred[3]
-            score = pred[2]
-            if x0 < bottom_center[0] < x1 and y0 < bottom_center[1] - 10 < y1:
-                square_info['piece'] = label_list[label]
-                square_info['score'] = float(score)
-                # print(f"Square: {square_name}, Piece: {label_list[label]}, Score: {score}")
-                break
+        predictions = []
+        for detection in detections:
+            xyxy = detection[0]
+            bottom_centre = ((xyxy[0] + xyxy[2]) / 2, xyxy[3])
+            label = detection[3]
+            score = detection[2]
+            if x0 < bottom_centre[0] < x1 and y0 < bottom_centre[1] - 10 < y1:
+                predictions.append((label_list[label], score))
+        predictions.sort(key=operator.itemgetter(1), reverse=True)
+        predictions = [label for label, _ in predictions]
+        square_info['predictions'] = predictions
 
-def dict_to_fen(square_dict):
-    # store FEN ranks in list to build FEN
-    fen_ranks = []
+def get_fen_permutations(square_dict):
+    """
+    Generate FEN permutations based on square predictions.
 
-    # FEN begins at rank 8, file a and goes down to rank 1, file h i.e. a8, b8... h1
-    # iterate through ranks, counting empty files and noting pieces to build file strings
-    for rank in '87654321':
-        file_string = ''
-        empty_count = 0
+    Args:
+        square_dict (dict): A dictionary containing square information and predictions.
+    Returns:
+        A list of FEN strings representing all possible board configurations given the predictions.
+    """
 
-        for file in 'abcdefgh':
-            square = f'{file}{rank}'
-            piece = square_dict[square]['piece']
-            if piece is None:
-                empty_count += 1
-            else:
-                if empty_count > 0:
-                    file_string += str(empty_count)
-                    empty_count = 0
-                file_string += piece
-
-        if empty_count > 0:
-            file_string += str(empty_count)
-
-        fen_ranks.append(file_string)
+    def board_to_fen(board):
+        """Build a FEN string from a nested list representation of the chessboard."""
+        # store FEN ranks in list to build FEN
+        fen_ranks = []
+        # FEN begins at rank 8, file a and goes down to rank 1, file h i.e. a8, b8... h1
+        # iterate through ranks, counting empty files and noting pieces to build rank strings
+        for rank in board:
+            rank_string = ''
+            empty_count = 0
+            for square in rank:
+                if square == "":
+                    empty_count += 1
+                else:
+                    if empty_count > 0:
+                        rank_string += str(empty_count)
+                        empty_count = 0
+                    rank_string += square
+            if empty_count > 0:
+                rank_string += str(empty_count)
+            fen_ranks.append(rank_string)
+            
+        return '/'.join(fen_ranks)
     
-    return '/'.join(fen_ranks)
+    board = [["" for _ in range(8)] for _ in range(8)]
+    file_to_index = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+    rank_to_index = {'1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
+    squares_with_alts = []
+
+    for square in square_dict:
+        file = file_to_index[square[0]]
+        rank = rank_to_index[square[1]]
+        predictions = square_dict[square]['predictions']
+        #print(f"Square: {square}, Predictions: {predictions}")
+        if len(predictions) > 1:
+            squares_with_alts.append((rank, file, predictions))
+        else:
+            board[rank][file] = predictions[0] if predictions else ""
+
+    fen_permutations = []
+
+    alts = [square[2] for square in squares_with_alts]
+    for combination in itertools.product(*alts):
+        temp_board = [row[:] for row in board]
+        for (rank, file, _), alt in zip(squares_with_alts, combination):
+            temp_board[rank][file] = alt
+        fen_permutations.append(board_to_fen(temp_board))
+
+    for fen in fen_permutations:
+        if fen.count('K') > 1 or fen.count('k') > 1 or fen.count('P') > 8 or fen.count('p') > 8:
+            fen_permutations.remove(fen)
+
+    return fen_permutations
