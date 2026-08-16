fd70a20250ca11e2767c85a3f53552cf62e2e019
b1leygr <okeferanmi10@gmail.com>
2026-08-16 18:39:03 +0100

    update main.py with revised fen prediction and multi-image  and evaluation functionality

--- main.py
+++ main.py
@@ -1,40 +1,104 @@
-from utils import *
 from pathlib import Path
+
+import cv2
 from rfdetr import RFDETRMedium
+
+from app.backend import chess_utils
 
 def main():
     project_dir = Path.cwd()
-    print(project_dir)
+    model = RFDETRMedium(
+        pretrain_weights=str(
+            project_dir
+            / 'models'
+            / 'rfdetr_medium_v2'
+            / 'checkpoint_best_total.pth'
+        ),
+        num_classes=12
+    )
+
     while True:
-        file = input('Choose an image file:') + '.jpg'
-        img = cv2.imread(project_dir / "notebooks" / "game_opera_side" / file)
-        try:
-            img = cv2.imread(project_dir / "notebooks" / "game_opera_side" / file)
-            print(f"{img} found!")
+        mode = input('Choose mode (1: single image, 2: batch images, 3: exit): ')
+
+        if mode == '1':
+            file = input('Choose an image file:') + '.jpg'
+            file = next((project_dir/'notebooks').rglob(f'**/{file}'), None)
+            if not file:
+                print('File not found.')
+                continue
+            move_id = file.stem.split('_')[2]
+            print(f'Processing image: {file}')
+            try:
+                true_fens = file.parent / 'fens.csv'
+                img = cv2.imread(file)
+                board_grid = chess_utils.localise_and_extract(img)
+                square_dict = chess_utils.squares_to_dict(board_grid)
+                detections = model.predict(img)
+                chess_utils.predict_board(square_dict, detections)
+                fens = chess_utils.get_fen_permutations(square_dict)
+                if not fens:
+                    print('No valid FENs found.')
+                else:
+                    with open(true_fens, 'r') as true_fens_file:
+                        true_fens_file.readline()
+                        for line in true_fens_file:
+                            if line.split(',')[0] == move_id:
+                                true_fen = line.split(',')[1].strip().split(' ')[0]
+                                if true_fen in fens:
+                                    if len(fens) > 1:
+                                        print(f'A predicted FEN matches the true FEN! {true_fen}')
+                                    else:
+                                        print(f'FEN matches the true FEN! {true_fen}')
+                continue
+            except Exception as e:
+                print(f'Error: {e}. Please choose a different image file.')
+
+        elif mode == '2':
+            dir_path = project_dir / 'notebooks' / input('Enter directory path for batch images: ')
+            if not dir_path.exists() or not dir_path.is_dir():
+                print('Directory not found.')
+                continue
+            print(f'Processing images in directory: {dir_path}')
+            try:
+                true_fens = dir_path / 'fens.csv'
+                fen_accuracy = {'game': dir_path.stem,
+                                'total_images': 0,
+                                'correct_predictions': 0}
+                for file in dir_path.glob('*.jpg'):
+                    fen_accuracy['total_images'] += 1
+                    move_id = file.stem.split('_')[2]
+                    img = cv2.imread(file)
+                    board_grid = chess_utils.localise_and_extract(img)
+                    square_dict = chess_utils.squares_to_dict(board_grid)
+                    detections = model.predict(img)
+                    chess_utils.predict_board(square_dict, detections)
+                    fens = chess_utils.get_fen_permutations(square_dict)
+                    if not fens:
+                        print('No valid FENs found.')
+                    else:
+                        with open(true_fens, 'r') as true_fens_file:
+                            true_fens_file.readline()
+                            for line in true_fens_file:
+                                if line.split(',')[0] == move_id:
+                                    true_fen = line.split(',')[1].strip().split(' ')[0]
+                                    if true_fen in fens:
+                                        if len(fens) > 1:
+                                            print(f'A predicted FEN matches the true FEN! {true_fen}')
+                                            fen_accuracy['correct_predictions'] += 1/len(fens)
+                                        else:
+                                            print(f'FEN matches the true FEN! {true_fen}')
+                                            fen_accuracy['correct_predictions'] += 1
+                print(f'Game: {fen_accuracy["game"]}, '
+                      f'Total images: {fen_accuracy["total_images"]}, ' 
+                      f'Correct predictions: {fen_accuracy["correct_predictions"]}, ' 
+                      f'Accuracy: {fen_accuracy["correct_predictions"]/fen_accuracy["total_images"]:.2%}')
+                continue
+            except Exception as e:
+                print(f'Error: {e}. Please choose a different directory path.')
+        
+        elif mode == '3':
+            print('Exiting...')
             break
-        except:
-            print(f"{file} not found. Please choose a different image file.")
-    blur = preprocess(img)
-    canny = autocanny(blur)
-    dilated = dilate (canny)
-    points = find_contour(dilated)
-    corners = find_corners(points)
-    warped, M_inv = warp(img, corners)
-    blur = preprocess(warped)
-    canny = autocanny(blur)
-    closed = close(canny)
-    lines = hough_lines(closed)
-    h, v = sort_lines(lines)
-    intersections = find_intersections(h, v)
-    clustered_points = cluster_intersections(intersections)
-    corners = find_corners(clustered_points)
-    squares = get_squares(corners, M_inv)
-    square_dict = squares_to_dict(squares)
-    model = RFDETRMedium(pretrain_weights=str(project_dir / 'models' / 'rfdetr_medium_v2' / 'checkpoint_best_total.pth'), num_classes=12)
-    predictions = model.predict(img)
-    predict_board(square_dict, predictions)
-    fen = dict_to_fen(square_dict)
-    print(fen)
 
 
 if __name__ == "__main__":
